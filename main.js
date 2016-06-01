ae633bc650a49ca8530838e5422290b7e9805b97
hunterkorvis <hunterkorvis@gmail.com>
2016-06-01 14:58:44 -0700

    fixed?
    trying to fix conflicts

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
-
 function rangeDisplay () {
 	var x = document.getElementById('myRange').value;
 		document.getElementById('rangeDisplayResult').innerHTML = 'Current range selection ' + x;
 }
 
-function handleConversion () {
-  var gpmInput = document.getElementById('GPMInput');
-  var result = document.getElementById('Result');
-  var range = document.getElementById('myRange').value;
-  var gpm = gpmInput.value;
-      result.innerHTML = 'Acre-feet Per Year (ac ft/yr): ' + convert(gpm, range);
-  
+function handleConversion (id, conversionFunction, message) {
+
+  // getElementsByTagName returns an array
+  // we know we only have one element of this tag type so we take the first item in the array [0]
+
+  var element = document.getElementById(id);
+  var input = element.getElementsByTagName('input')[0];
+  var button = element.getElementsByTagName('button')[0];
+  var result = element.getElementsByTagName('p')[0];
+
+  // tell the button what function to use when clicked
+  button.onclick = function () {
+    result.innerHTML = message + conversionFunction(input.value);
+  };
 }
 
-function handleConversion2 () {
-  var AFYInput = document.getElementById('AFYInput');
-  var result2 = document.getElementById('Result2');
-  var range = document.getElementById('myRange').value;
-  var afy = AFYInput.value;
-      result2.innerHTML = 'Gallons (us Fluid) Per Minute (GPM): ' + convert2(afy, range);
-  
+function gpmToAfy (gpm) {
+  return (gpm * 1.61408).toFixed(5);
 }
 
-function convert (value,decimals) {
-  // conversion function
-  return (value * 1.61408).toFixed(decimals);
+function afyToGpm (afy) {
+  return (afy * 0.61955).toFixed(5);
 }
 
-function convert2 (value2,decimals) {
-  // conversion function
-  return (value2 * 0.61955).toFixed(decimals);
-}
+// Get elements once DOM has loaded
+// Sometimes the JS fires before the DOM is fully loaded, so we wait for the DOM to tell us it's ready
+document.addEventListener('DOMContentLoaded', function () {
+
+  // handleConversion takes ID, function, message
+  handleConversion('GPM', gpmToAfy, 'Acre-feet Per Year (ac ft/yr): ');
+  handleConversion('AFY', afyToGpm, 'Gallons (us Fluid) Per Minute (GPM): ');
+
+}, false);
